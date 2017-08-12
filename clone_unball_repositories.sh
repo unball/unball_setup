2dfceda1cc3378c6054a5bbaa3e084b43a607575
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-08-12 17:18:28 -0300

    Created repo cloning and simulator download scripts

---
+++ clone_unball_repositories.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/unball/ieee-very-small.git
+git clone https://github.com/unball/vision.git
+git clone https://github.com/unball/communication.git
+git clone https://github.com/unball/strategy.git
---
+++ donwload_simulator.sh
@@ -0,0 +1,9 @@
+# Setup folder
+mkdir simulator
+cd simulator
+
+# Download latest version from Google Drive
+wget -O "unball_simulator.tar.gz" "https://drive.google.com/uc?export=download&id=0BwlvQGynHcxZTTdPUnF3dGR0MlE"
+
+# Extract downloaded version, overwriting files
+tar -xzf "unball_simulator.tar.gz" --overwrite
