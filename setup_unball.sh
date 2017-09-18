dd2762eb30849bcc2c88b8fa70373f2c0388a30f
Vieira Neto <vieiranetoc@gmail.com>
2017-09-18 19:36:06 -0300

    made setup dynamic

--- setup_unball.sh
+++ setup_unball.sh
@@ -3,21 +3,30 @@ BLUE='\033[0;34m'
 GREEN='\033[0;32m'
 NO_COLOR='\033[0m'
 
-ORIGINAL_DIRECTORY=$PWD
-cd ~
-if [ -d "unball" ]
-then
-    printf "${RED}~/unball folder already exists, nothing done.${NO_COLOR}\n"
-else
-    printf "\n${BLUE}Cloning unball repositories in folder ~/unball${NO_COLOR}\n\n"
-    mkdir ~/unball
-    cd ~/unball
-    git clone https://github.com/unball/ieee-very-small.git
-    git clone https://github.com/unball/vision.git
-    git clone https://github.com/unball/communication.git
-    git clone https://github.com/unball/strategy.git
-    git clone https://github.com/unball/control.git
+repos=(
+    ieee-very-small
+    vision
+    communication
+    control
+    measurement-system
+)
 
+check_repos(){
+    printf "\n${BLUE} Checking for packages existence${NO_COLOR}"
+    declare -a argAry=("${!1}")
+    clonestr='git clone https://github.com/unball/'
+    for i in "${argAry[@]}"
+    do
+        if [[ ! -d "$i" ]] ; then
+            printf "${GREEN} Cloning repo $i${NO_COLOR}\n"
+            $clonestr$i
+        else
+            printf "${BLUE} $i already cloned ${NO_COLOR}\n"
+        fi
+    done
+}
+
+download_simulator(){
     printf "\n${BLUE}Downloading the unball simulator${NO_COLOR}\n\n"
     mkdir ~/unball/simulator
     cd ~/unball/simulator
@@ -25,22 +34,51 @@ else
     wget -O "unball_simulator.tar.gz" "https://drive.google.com/uc?export=download&id=0BwlvQGynHcxZTTdPUnF3dGR0MlE"
     # Extract downloaded version, overwriting files
     tar -xzf "unball_simulator.tar.gz" --overwrite
+}
+
+configure_catkin(){
+    declare -a argAry=("${!1}")
 
     printf "\n${BLUE}Setting up catkin workspace at catkin_ws_unball${NO_COLOR}\n\n"
-    mkdir ~/catkin_ws_unball
+    mkdir -p ~/catkin_ws_unball
     cd ~/catkin_ws_unball
-    mkdir src
-    catkin_make
-    ln -s ~/unball/ieee-very-small ~/catkin_ws_unball/src/ieee-very-small
-    ln -s ~/unball/vision ~/catkin_ws_unball/src/vision
-    ln -s ~/unball/strategy ~/catkin_ws_unball/src/strategy
-    ln -s ~/unball/communication ~/catkin_ws_unball/src/communication
-    ln -s ~/unball/simulator ~/catkin_ws_unball/src/simulator
-    ln -s ~/unball/control ~/catkin_ws_unball/src/control
+    mkdir -p src
+    catkin_make -j1
+    for i in "${argAry[@]}"
+    do
+        if [[ ! -e "src/"$i ]]; then
+            ln -s ~/unball/$i ~/catkin_ws_unball/src/$i
+        fi
+    done
     cp ${ORIGINAL_DIRECTORY}/run_strategy_and_simulator.sh ~/catkin_ws_unball/
     cp ${ORIGINAL_DIRECTORY}/update_all_repos.sh ~/catkin_ws_unball/
-
+    chmod 777 ${ORIGINAL_DIRECTORY}/run_strategy_and_simulator.sh
+    sudo cp ${ORIGINAL_DIRECTORY}/run_strategy_and_simulator.sh /usr/bin
     catkin_make;
 
     printf "\n${BLUE}Setup complete! This folder may now be deleted if you wish.${NO_COLOR}\n"
+}
+
+ORIGINAL_DIRECTORY=$PWD
+cd ~
+
+if [[ ! -d "~/unball" ]]; then
+    mkdir -p ~/unball
 fi
+
+cd ~/unball
+check_repos repos[@]
+
+if [[ $1 != "-y" ]]; then
+    printf "Do you want to download simulator?[y/N] "
+    read choice
+    if [[($choice == "y") || ($choice == "Y") ]]; then
+        download_simulator
+    fi
+else
+    download_simulator
+fi
+
+configure_catkin repos[@]
+
+
