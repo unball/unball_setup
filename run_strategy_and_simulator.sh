5ea1bf4a72978d65c1299e0dc0b653d246877ca8
sousasmurf <g.sousasmurf@gmail.com>
2017-10-06 13:20:50 -0300

    changed script go_to_ball to start_strategy

--- run_strategy_and_simulator.sh
+++ run_strategy_and_simulator.sh
@@ -4,7 +4,7 @@ cd ~/catkin_ws_unball
 rossource='source /opt/ros/kinetic/setup.bash; source ~/catkin_ws_unball/devel/setup.bash'
 rosbridge='bash -c "rossubs; roslaunch rosbridge_server rosbridge_websocket.launch; bash"'
 simulator='bash -c "./src/simulator/unball_simulator.x86_64; bash"'
-go_to_ball='bash -c "rossubs; rosrun strategy go_to_ball.py; bash"'
+start_strategy='bash -c "rossubs; rosrun strategy start_strategy.py; bash"'
 measurement='bash -c "rossubs; rosrun measurement_system kalman.py; bash"'
 relative_position_converter='bash -c "rossubs; rosrun strategy relative_position_converter.py; bash"' 
 position_control='bash -c "rossubs; python2 ./src/control/position_control.py; bash"'
@@ -12,6 +12,6 @@ position_control='bash -c "rossubs; python2 ./src/control/position_control.py; b
 gnome-terminal \
     --tab -e "${rosbridge//rossubs/$rossource}" \
     --tab -e "${simulator}" \
-    --tab -e "${go_to_ball//rossubs/$rossource}"\
+    --tab -e "${start_strategy//rossubs/$rossource}"\
     --tab -e "${relative_position_converter//rossubs/$rossource}"\
     --tab -e "${position_control//rossubs/$rossource}" \
