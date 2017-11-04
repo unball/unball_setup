5486e4a373943b2397f28b80317f6033296c6754
sousasmurf <g.sousasmurf@gmail.com>
2017-11-04 04:05:38 -0200

    measurment added in run strategy and simulator script

--- run_strategy_and_simulator.sh
+++ run_strategy_and_simulator.sh
@@ -5,7 +5,7 @@ rossource='source /opt/ros/kinetic/setup.bash; source ~/catkin_ws_unball/devel/s
 rosbridge='bash -c "rossubs; roslaunch rosbridge_server rosbridge_websocket.launch; bash"'
 simulator='bash -c "./src/simulator/unball_simulator.x86_64; bash"'
 start_strategy='bash -c "rossubs; rosrun strategy start_strategy.py; bash"'
-measurement='bash -c "rossubs; rosrun measurement_system kalman.py; bash"'
+measurement='bash -c "rossubs; rosrun measurement_system measurement.py; bash"'
 relative_position_converter='bash -c "rossubs; rosrun strategy relative_position_converter.py; bash"' 
 control_system='bash -c "rossubs; rosrun control control_system.py; bash"'
 
@@ -13,6 +13,7 @@ control_system='bash -c "rossubs; rosrun control control_system.py; bash"'
 gnome-terminal \
     --tab -e "${rosbridge//rossubs/$rossource}" \
     --tab -e "${simulator}" \
+    --tab -e "${measurement//rossubs/$rossource}" \
     --tab -e "${start_strategy//rossubs/$rossource}"\
     --tab -e "${relative_position_converter//rossubs/$rossource}"\
     --tab -e "${control_system//rossubs/$rossource}" \
