0aa710addee1d993e0fe1cf32dde8aa420e93860
sousasmurf <g.sousasmurf@gmail.com>
2017-11-05 10:27:42 -0200

    run script updated with new strategy

--- run_strategy_and_simulator.sh
+++ run_strategy_and_simulator.sh
@@ -4,7 +4,7 @@ cd ~/catkin_ws_unball
 rossource='source /opt/ros/kinetic/setup.bash; source ~/catkin_ws_unball/devel/setup.bash'
 rosbridge='bash -c "rossubs; roslaunch rosbridge_server rosbridge_websocket.launch; bash"'
 simulator='bash -c "./src/simulator/unball_simulator.x86_64; bash"'
-start_strategy='bash -c "rossubs; rosrun strategy start_strategy.py; bash"'
+start_strategy='bash -c "rossubs; rosrun strategy arch_strategy.py; bash"'
 measurement='bash -c "rossubs; rosrun measurement_system measurement.py; bash"'
 relative_position_converter='bash -c "rossubs; rosrun strategy relative_position_converter.py; bash"' 
 control_system='bash -c "rossubs; rosrun control control_system.py; bash"'
